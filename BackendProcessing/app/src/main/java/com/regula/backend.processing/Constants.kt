bfe97092eb85a6e0d634f59a8381055e41c10736
benpaulson523 <ben@neuvote.com>
2026-01-05 08:38:36 -0500

    Switch to HTTPS server and disable cleartext traffic
    Updated NEUVOTE_SERVER_ADDRESS to use the new HTTPS endpoint and set cleartextTrafficPermitted to false in network security config for improved security. Also removed example hints from server and port input fields in the settings layout.

--- BackendProcessing/app/src/main/java/com/regula/backend.processing/Constants.kt
+++ BackendProcessing/app/src/main/java/com/regula/backend.processing/Constants.kt
@@ -16,8 +16,9 @@ object Constants {
     const val FUEL_URL = "https://sg.rp.secure.iproov.me/api/v2/"
     const val API_KEY = "8a22f4928c1191a8f1c21ed8b8ca4871fc5b4fb7"
     const val SECRET = "0cdeeca2ae00d68fb25e0c0aeacf63c61d3c0f44"
-    //const val NEUVOTE_SERVER_ADDRESS = "http://192.168.0.228"
-    //const val NEUVOTE_SERVER_PORT = "8080"
-    const val NEUVOTE_SERVER_ADDRESS = "http://biometrics-demo-env.eba-tphgd5hp.ca-central-1.elasticbeanstalk.com"
+    //const val NEUVOTE_SERVER_ADDRESS = "http://192.168.0.228" //local
+    //const val NEUVOTE_SERVER_PORT = "8080"                    //local
+    //const val NEUVOTE_SERVER_ADDRESS = "http://biometrics-demo-env.eba-tphgd5hp.ca-central-1.elasticbeanstalk.com"    //http
+    const val NEUVOTE_SERVER_ADDRESS = "https://api.biometrics.demo.civik.ca"                                           //https
     const val NEUVOTE_SERVER_PORT = ""
 }
